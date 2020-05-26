c5e489ff1af71008fe95a1d028c850c93e2235bf
liulingzhi <136617371@qq.com>
2020-05-26 12:27:06 +0800

    seq添加 attribute_features参数

--- sequence/seq.py
+++ sequence/seq.py
@@ -1,7 +1,7 @@
 '''
 @Author: your name
 @Date: 2020-05-25 18:18:20
-@LastEditTime: 2020-05-26 10:15:23
+@LastEditTime: 2020-05-26 12:26:11
 @LastEditors: Please set LastEditors
 @Description: In User Settings Edit
 @FilePath: /dataprocess/sequence/seq.py
@@ -12,15 +12,17 @@ import pandas as pd
 import numpy as np
 
 
-def make_seq(x, groupby_feature, behavior_features, time_feature, max_len):
+def make_hist_sequences(data, groupby_feature, behavior_features, attribute_features, time_feature, max_len, keep_size=False):
     """
     Parameters
     ----------
-    x: pd.DataFrame
+    data: pd.DataFrame
 
     groupby_feature: str, 计算user的序列还是item的序列，直接传入列名
 
     behavior_features: 计算哪些行为的序列，[店铺id， 品类id， 商品id]
+
+    attribute_features: 持续不变的特征，一个user_id只有一个
 
     time_feature: 时间特征列，需要先按照这里进行排序
 
@@ -28,39 +30,49 @@ def make_seq(x, groupby_feature, behavior_features, time_feature, max_len):
 
     """
     # 在numpy层面进行计算，速度快
-    if isinstance(behavior_features, list):
-        features = [groupby_feature] + behavior_features
+    if not isinstance(behavior_features, list) or not isinstance(attribute_features, list):
+        raise ValueError('behavior_features 和  attribute_features必须为列表')
     else:
-        features = [groupby_feature, behavior_features]
+        features = [groupby_feature] + behavior_features + attribute_features
 
-    key_value_list = x[features].sort_values(
+    feature_value_list = data[features].sort_values(
         [groupby_feature, time_feature]).values.T
 
-    keys = key_value_list[0]
-    ukeys, index = np.unique(keys, True)
+    groupby_feature_value = feature_value_list[0]
+    groupby_feature_unique_value, groupby_feature_change_index = np.unique(groupby_feature_value, True)
     # 以groupby_feature作为第一排序，然后直接找到每次groupby_feature变化时候的index
 
-    arrays = [np.split(values, index[1:]) for values in key_value_list[1:]]
-    hist_dict = {
-        'hist_'+behavior_features[i]:
-         [list(a[:max_len])  if len(a)>max_len else list(a) + [0]*(max_len - len(a)) for a in arrays[i]]
-          for i, feat in enumerate(behavior_features)}
-    hist_dict[groupby_feature] = ukeys
-    
-    hist_mapper = pd.DataFrame(hist_dict)
+    behavior_features_values = [np.split(values, groupby_feature_change_index[1:]) for values in feature_value_list[1: 1+len(behavior_features)]]    
+    attribute_features_values = [values[groupby_feature_change_index] for values in feature_value_list[1+len(behavior_features): ]]
 
-    x.set_index(groupby_feature, inplace=True)
+    print(len(feature_value_list))
+    print(len(behavior_features_values))
+    print(len(attribute_features_values))
+
+    hist_dict = {}
+    hist_dict[groupby_feature] = groupby_feature_unique_value
+    for idx, feat in enumerate(behavior_features):
+        hist_dict['hist_'+feat] = [list(behavior_seq[:max_len]) for behavior_seq in behavior_features_values[idx]]
+    for idx, feat in enumerate(attribute_features):
+        hist_dict[feat] = [v for v in attribute_features_values[idx]]
+
+    hist_mapper = pd.DataFrame(hist_dict)
+    if not keep_size:
+        return hist_mapper
+
+    data.set_index(groupby_feature, inplace=True)
     hist_mapper.set_index(groupby_feature, inplace=True)
-    x = x.merge(hist_mapper, left_index=True, right_index=True)
-    x = x.reset_index()
-    # x = x.merge(hist_mapper, on=groupby_feature, how='left')
-    return x
+    data = data.merge(hist_mapper, left_index=True, right_index=True)
+    data = data.reset_index()
+    # data = data.merge(hist_mapper, on=groupby_feature, how='left')
+    return data
 
 
 data = pd.DataFrame(
     {
-        'a': list(range(1000000)), 
+        'a': list(range(0, 1000000, 2)) * 2, 
         'b': list(range(1000000)), 
         'c': list(range(1000000))})
+# print(data)
+print(make_hist_sequences(data, 'a', ['b'], ['c'], 'a', 100))
 
-print(make_seq(data, 'a', ['b', 'c'], 'a', 100))
